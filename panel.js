1b4451f8842a26b453a02966da96b770e10516b4
Pranay Dutta <pranaydutta89@gmail.com>
2018-11-14 22:34:24 +0530

    screenshot enabled

--- panel.js
+++ panel.js
@@ -3,6 +3,9 @@ chrome.runtime.onMessage.addListener(
         console.log("background.js got a message")
         console.log(request);
         console.log(sender);
-
+        chrome.tabs.captureVisibleTab(null, {}, function (image) {
+            // You can add that image HTML5 canvas, or Element.
+            debugger
+        });
     }
 );
